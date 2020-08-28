9979867a3e036759feb823e44cd9f85f55054a9e
Paul Clarke <paulioclarke@icloud.com>
2020-08-28 17:55:23 +1000

    app

---
+++ build/precache-manifest.99dc8d6383abec4d6148e140421b40bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f767f5a317e0c5fc4a3fa560ec98222d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af1f1d203220f7d438c",
+    "url": "/static/css/main.8cf0df49.chunk.css"
+  },
+  {
+    "revision": "d8ac341b042f6571b9b7",
+    "url": "/static/js/2.7d7ae3f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7d7ae3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af1f1d203220f7d438c",
+    "url": "/static/js/main.7929fca1.chunk.js"
+  },
+  {
+    "revision": "c2d3ac118f5c9e06f79d",
+    "url": "/static/js/runtime-main.f7154132.js"
+  },
+  {
+    "revision": "c8ff2c80bc9631377817bc48796688e4",
+    "url": "/static/media/white_circle.c8ff2c80.png"
+  }
+]);
